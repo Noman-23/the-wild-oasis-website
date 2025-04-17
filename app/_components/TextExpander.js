b005896858c257c829dd9de6e322594ba39ca0b2
Noman-23 <mohammadnoman2311@gmail.com>
2025-04-17 15:43:24 +0530

    inital commit

---
+++ app/_components/TextExpander.js
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+
+function TextExpander({ children, words = 40 }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const displayText = isExpanded ? children : children.split(' ').slice(0, words).join(' ') + '...';
+
+  return (
+    <span>
+      {displayText}
+      <button
+        className='text-primary-700 border-b border-primary-700 leading-3 pb-1 ml-2'
+        onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? 'Show less' : 'Show more'}
+      </button>
+    </span>
+  );
+}
+
+export default TextExpander;
